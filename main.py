893efd487b35260aac6de788debd6acd409c4e7a
OliverUrsell <ollie@fuzzylabs.ai>
2022-04-12 16:29:14 +0100

    Implement a shap example which saves a summary plot

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
+import sklearn
+import shap
+import matplotlib.pyplot as plt
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# print the JS visualization code to the notebook
+shap.initjs()
 
+# train a SVM classifier
+X_train,X_test,Y_train,Y_test = sklearn.model_selection.train_test_split(*shap.datasets.iris(), test_size=0.2, random_state=0)
+svm = sklearn.svm.SVC(kernel='rbf', probability=True)
+svm.fit(X_train, Y_train)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# use Kernel SHAP to explain test set predictions
+explainer = shap.KernelExplainer(svm.predict_proba, X_train, link="logit")
+shap_values = explainer.shap_values(X_test, nsamples=100)
 
+# print(shap_values)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# plot the SHAP values for the Setosa output of all instances
+# This commented code seems to just save a white image
+# fig = shap.force_plot(explainer.expected_value[0], shap_values[0], X_test, link="logit")
+# plt.savefig('grafic.png')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+fig = shap.summary_plot(shap_values, X_train, show=False)
+plt.savefig('shap.png', bbox_inches='tight')
+
